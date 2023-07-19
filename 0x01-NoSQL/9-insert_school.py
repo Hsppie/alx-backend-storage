27a3d3c7571cf60eb6c6d95dbfa91675dcb08253
Hsppie <wambimicheal@gmail.com>
2023-07-19 23:08:11 +0300

    Fix Debug

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,6 +1,10 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
+'''Task 9: Insert a document in Python
+'''
 
 
-def insert_scholl(monogo_collection, **kwargs):
-    result = mongo_collection.insertOne(kwargs)
+def insert_school(mongo_collection, **kwargs):
+    '''Inserts a new document in a collection.
+    '''
+    result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
